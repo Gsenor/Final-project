b1124cb668904f10309a12e11e7e6f87b64e18e6
Gsenor <gase7920@colorado.edu>
2016-04-27 19:28:10 -0600

    This is the class that is used in main

---
+++ CommunicationNetwork.h
@@ -0,0 +1,45 @@
+#ifndef COMMUNICATIONNETWORK_H
+#define COMMUNICATIONNETWORK_H
+#include <iostream>
+struct City{
+    std::string cityName;
+    std::string message;
+    City *next;
+    City *previous
+
+    City(){}; // default constructor
+
+    City(std::string initName, City *initNext, std::string initMessage, City *initPrevious)
+    {
+        cityName = initName;
+        next = initNext;
+        previous = initPrevious
+        message = initMessage;
+    }
+
+};
+
+class CommunicationNetwork
+{
+    public:
+        CommunicationNetwork();
+        ~CommunicationNetwork();
+        void addCity(std::string, std::string);
+        void buildNetwork();
+        void transmitMsg(char *); //this is like a string
+        void printNetwork();
+        void deleteCity(std::string);
+        void showMailPath(std::string, std::string);
+        int countTotalCities();
+        int countCitiestransit(std::string, std::string);
+        void transmitmessback(char *);
+        void searchNetwork(std::string)
+        
+        
+    protected:
+    private:
+        City *head;
+        City *tail;
+};
+
+#endif 
